d910a0ebba2287b37eeba69c3d896011fd380b08
Akash-2911 <2911akashpatel@gmail.com>
2026-01-22 13:44:41 -0700

    Week 2 assignment complete

---
+++ app/week-2/page.tsx
@@ -0,0 +1,10 @@
+import StudentInfo from "./student.info";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Week 2 Assignment</h1>
+      <StudentInfo />
+    </div>
+  );
+}
---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Akash Patel</p>
+
+      <Link href="https://github.com/YOUR-GITHUB-USERNAME/cprg306-assignments">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
